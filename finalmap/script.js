bca4f58b17aea079cf694a4cce249af7f6c026d0
4ouR04 <amosmwongelah@gmail.com>
2023-01-16 16:17:58 +0300

    Fix title

--- finalmap/script.js
+++ finalmap/script.js
@@ -1,30 +1,7 @@
-// "use strict"
+"use strict"
 
-// const person = {
-//     fname: 'Amos',
-//     lname: 'Mwongela',
-//     get fullname(){
-//         return this.fname + " " + this.lname
-//     },
-//     fllname: function(){
-//         return this.fname + " " + this.lname
-//     }
-// }
+const playnow = document.querySelector("#playnow")
 
-// console.log(person.fllname())
-
-class person {
-    constructor(fname, lname, year){
-        this.fname = fname
-        this.lname = lname
-        this.year = year
-    }
-    get Age(){
-        let date = new Date()
-        return date.getFullYear() - this.year;
-    }
-}
-
-const person1 = new person("Amos", "Mwongela", 2001)
-
-console.log(person1.Age)
+playnow.addEventListener("click",  ()=>{
+    
+})
